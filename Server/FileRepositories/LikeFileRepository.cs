36281d206975defd99b9e2df47ed73abb47148e4
betelgeuseBet <151634373+betelgeuseBet@users.noreply.github.com>
2024-09-20 21:23:56 +0300

    Added LikeFileRepository

--- Server/FileRepositories/LikeFileRepository.cs
+++ Server/FileRepositories/LikeFileRepository.cs
@@ -1,33 +1,67 @@
-﻿using Entities;
+﻿using System.Text.Json;
+using Entities;
 using RepositoryContracts;
 
 namespace FileRepositories;
 
 public class LikeFileRepository:ILikeRepository
 {
-    private ILikeRepository _likeRepositoryImplementation;
-    public Task<Like> AddLikeAsync(Like like)
+    private readonly string filePath = "likes.json";
+
+    public async Task<Like> AddLikeAsync(Like like)
     {
-        return _likeRepositoryImplementation.AddLikeAsync(like);
+        List<Like> likes = await LoadLikesAsync();
+        int maxId = likes.Count > 0 ? likes.Max(like => like.LikeId):1;
+        like.LikeId = maxId + 1;
+        likes.Add(like);
+        SaveLikesAsync(likes);
+        return like;
     }
 
-    public Task DeleteLikeAsync(Like like)
+    public async Task DeleteLikeAsync(Like like)
     {
-        return _likeRepositoryImplementation.DeleteLikeAsync(like);
+        List<Like> likes = await LoadLikesAsync();
+        Like likeToDelete = await GetLikeByIdAsync(like.LikeId);
+        likes.Remove(likeToDelete);
+        SaveLikesAsync(likes);
     }
 
-    public Task<Like> GetLikeByIdAsync(int id)
+    public async Task<Like> GetLikeByIdAsync(int id)
     {
-        return _likeRepositoryImplementation.GetLikeByIdAsync(id);
+        List<Like> likes = await LoadLikesAsync();
+        Like? like = likes.SingleOrDefault(l => l.LikeId == id);
+        if (like is null)
+            throw new InvalidOperationException($"No like with ID {id}.");
+        return like;
     }
 
     public IQueryable<Like> GetLikesForPost(int postId)
     {
-        return _likeRepositoryImplementation.GetLikesForPost(postId);
+        List<Like> likes = LoadLikesAsync().Result;
+        List<Like> likesForPost =
+            likes.Where(like => like.PostId == postId).ToList();
+        if(likesForPost.Count==0)
+            throw new InvalidOperationException("No likes for this post.");
+        
+        return likesForPost.AsQueryable();
     }
 
     public IQueryable<Like> GetAllLikes()
     {
-        return _likeRepositoryImplementation.GetAllLikes();
+        List<Like> likes = LoadLikesAsync().Result;
+        return likes.AsQueryable();
+    }
+    private async Task<List<Like>> LoadLikesAsync()
+    {
+        string likesAsJson = await File.ReadAllTextAsync(filePath);
+        List<Like> likes =
+            JsonSerializer.Deserialize<List<Like>>(likesAsJson)!;
+        return likes;
+    }
+
+    private async void SaveLikesAsync(List<Like> toSaveLikes)
+    {
+        string likesAsJson = JsonSerializer.Serialize(toSaveLikes);
+        await File.WriteAllTextAsync(filePath, likesAsJson);
     }
 }
