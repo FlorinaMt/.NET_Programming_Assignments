835a3329a745a1c5979c1efb37638ad841b06b2d
betelgeuseBet <151634373+betelgeuseBet@users.noreply.github.com>
2024-09-27 13:22:46 +0300

    Comment Reactions added. Created the WebAPI.

---
+++ Server/Entities/Reaction.cs
@@ -0,0 +1,9 @@
+﻿namespace Entities;
+
+public class Reaction
+{
+    public int ReactionId { get; set; }
+    public int UserId { get; set; }
+    public int CommentId { get; set; }
+    public bool IsPositive { get; set; }
+}
---
+++ Server/RepositoryContracts/IReactionRepository.cs
@@ -0,0 +1,12 @@
+﻿using Entities;
+
+namespace RepositoryContracts;
+
+public interface IReactionRepository
+{
+    Task<Reaction> AddReactionAsync(Reaction reaction);
+    Task DeleteReactionAsync(Reaction reaction);
+    Task<Reaction> GetReactionByIdAsync(int id);
+    IQueryable<Reaction> GetReactionsForComment(int commentId);
+    IQueryable<Reaction> GetAllReactions();
+}
