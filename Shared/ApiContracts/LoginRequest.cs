413edbe562ed576cd8999f8dd781becc87a062fa
betelgeuseBet <151634373+betelgeuseBet@users.noreply.github.com>
2024-11-11 08:34:01 +0100

    Auth server side

--- Shared/ApiContracts/LoginRequest.cs
+++ Shared/ApiContracts/LoginRequest.cs
@@ -1,7 +1,8 @@
 ﻿namespace ApiContracts;
 
-public class AddUserResponseDto
+public class LoginRequest
 {
     public string Username { get; set; }
-    public int UserId { get; set; }
+    
+    public string Password { get; set; }
 }
