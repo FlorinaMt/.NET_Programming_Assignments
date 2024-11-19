dcfa08d608c621b769a118accd8a862002972ba9
betelgeuseBet <151634373+betelgeuseBet@users.noreply.github.com>
2024-11-19 11:06:55 +0100

    Updated entities with foreign key for EFC

--- Server/Entities/Like.cs
+++ Server/Entities/Like.cs
@@ -3,6 +3,15 @@
 public class Like
 {
     public int LikeId { get; set; }
-    public int UserId { get; set; }
-    public int PostId { get; set; }
+    public User User { get; set; } = null!;
+    public Post Post { get; set; } = null!;
+
+    private Like()
+    {
+        
+    }
+    public static Like getLike()
+    {
+        return new Like();
+    }
 }
--- Server/Entities/User.cs
+++ Server/Entities/User.cs
@@ -4,38 +4,48 @@ public class User
 {
     public int UserId { get; set; }
     private string username, password;
-    public string Username {
-        get
-        {
-            return username;
-        }
+
+    public List<Post>? Posts { get; set; } = [];
+    public List<Comment>? Comments { get; set; } = [];
+    public List<Like>? Likes { get; set; } = [];
+
+    private User()
+    {
+    }
+
+    public static User GetUser()
+    {
+        return new User();
+    }
+
+    public string Username
+    {
+        get { return username; }
         set
         {
-            if(value is null || value.Trim().Equals(""))
+            if (value is null || value.Trim().Equals(""))
                 throw new ArgumentException("The username cannot be empty.");
             username = value;
         }
     }
 
-    public string Password {
-        get
-        {
-            return password;
-        }
+    public string Password
+    {
+        get { return password; }
         set
         {
-            if(value.Length<5 || value.Trim().Equals(""))
+            if (value.Length < 5 || value.Trim().Equals(""))
                 throw new ArgumentException("The password is too short.");
             password = value;
         }
-        
     }
+
 
     public override bool Equals(object? obj)
     {
         if (obj is null)
             return false;
-        User other=(User)obj;
+        User other = (User)obj;
         if (other.Username.Equals(Username) && other.Password.Equals(Password) &
             other.UserId == UserId)
             return true;
